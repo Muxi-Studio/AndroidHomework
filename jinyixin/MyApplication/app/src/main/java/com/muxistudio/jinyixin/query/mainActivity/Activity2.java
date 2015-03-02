8dfd049d5d1c54bbb2f404d8c96c33caee59066b
jinyixin <jinyixinseraph@gmail.com>
2015-03-02 11:50:29 +0800

    have bugs

--- jinyixin/MyApplication/app/src/main/java/com/muxistudio/jinyixin/query/mainActivity/Activity2.java
+++ jinyixin/MyApplication/app/src/main/java/com/muxistudio/jinyixin/query/mainActivity/Activity2.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.muxistudio.jinyixin.query.R;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -33,15 +39,18 @@ public class Activity2 extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
 
+        
         content = (TextView) findViewById(R.id.content);
         Intent receive = getIntent();
-        Bundle bundle_receive=receive.getExtras();
+        Bundle bundle_receive = receive.getExtras();
         String username = bundle_receive.getString("name");
         String tip = bundle_receive.getString("tip");
-        if(tip.equals("username"))
-        {
-        final String path = "https://www.v2ex.com/api/members/show.json?username=" + username;
-
+        final String path;
+        if (tip.equals("username")) {
+            path = "https://www.v2ex.com/api/members/show.json?username=" + username;
+        } else {
+            path = "https://www.v2ex.com/api/members/show.json?id=" + username;
+        }
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -64,71 +73,83 @@ public class Activity2 extends ActionBarActivity {
                     BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                     String line = null;
                     StringBuilder builder = new StringBuilder();
-                    while((line=bufferedReader.readLine()) != null)
-                    {
-                         builder.append(line);
+                    while ((line = bufferedReader.readLine()) != null) {
+                        builder.append(line);
                     }
                     temp = builder.toString();
-                    Log.i("result",temp);
+                    Log.i("result", temp);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             content.setText(temp);
                         }
                     });
-                }catch (IOException e) {
+
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
 
         });
         thread.start();
-        }else {
-            final String path = "https://www.v2ex.com/api/members/show.json?id=" + username;
 
-            Thread thread = new Thread(new Runnable() {
-                @Override
-                public void run() {
+//        JSONObject jsonObject = null;
+//        try {
+//            jsonObject = new JSONObject(temp);
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
+//        try {
+//            assert jsonObject != null;
+//            String statue1 = jsonObject.getString("statue");
+//            int id1 = jsonObject.getInt("id");
+//            String url1 = jsonObject.getString("url");
+//            String usernames1 = jsonObject.getString("username");
+//            String website1 = jsonObject.getString("website");
+//            String twitter1 = jsonObject.getString("twitter");
+//            String psn1 = jsonObject.getString("psn");
+//            String github1 = jsonObject.getString("github");
+//            String btc1 = jsonObject.getString("btc");
+//            String location1 = jsonObject.getString("location");
+//            String tagline1 = jsonObject.getString("tagline");
+//            String bio1 = jsonObject.getString("bio");
+//            String avatar_mini1 = jsonObject.getString("avatar_mini");
+//            EditText statue = (EditText) findViewById(R.id.statue);
+//            EditText id = (EditText) findViewById(R.id.id);
+//            EditText url = (EditText) findViewById(R.id.url);
+//            EditText usernames = (EditText) findViewById(R.id.usernames);
+//            EditText website = (EditText) findViewById(R.id.website);
+//            EditText twitter = (EditText) findViewById(R.id.twitter);
+//            EditText psn = (EditText) findViewById(R.id.psn);
+//            EditText github = (EditText) findViewById(R.id.github);
+//            EditText btc = (EditText) findViewById(R.id.btc);
+//            EditText location = (EditText) findViewById(R.id.location);
+//            EditText tagline = (EditText) findViewById(R.id.tagline);
+//            EditText bio = (EditText) findViewById(R.id.bio);
+//            EditText avatar_mini = (EditText) findViewById(R.id.avatar_mini);
+//            statue.setText(statue1);
+//            id.setText(id1);
+//            url.setText(url1);
+//            usernames.setText(usernames1);
+//            website.setText(website1);
+//            twitter.setText(twitter1);
+//            psn.setText(psn1);
+//            github.setText(github1);
+//            btc.setText(btc1);
+//            location.setText(location1);
+//            tagline.setText(tagline1);
+//            bio.setText(bio1);
+//            avatar_mini.setText(avatar_mini1);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
 
-                    try {
+        //content.setText(temp);
 
-                        URL url = new URL(path);
-
-                        HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-
-                        httpURLConnection.setConnectTimeout(10000);
-
-                        httpURLConnection.setRequestMethod("GET");
-
-                        httpURLConnection.setRequestProperty("Accept-Charset", "UTF-8");
-                        httpURLConnection.setRequestProperty("Accept-Language", Locale.getDefault().toString());
-
-                        InputStream inputStream = httpURLConnection.getInputStream();
-
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                        String line = null;
-                        StringBuilder builder = new StringBuilder();
-                        while((line=bufferedReader.readLine()) != null)
-                        {
-                            builder.append(line);
-                        }
-                        temp = builder.toString();
-                        Log.i("result",temp);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                content.setText(temp);
-                            }
-                        });
-                    }catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-            });
-            thread.start();
-        }
     }
+
+
 
 
     @Override
